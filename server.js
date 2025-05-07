a2b429adf97e19172736fdd4edc2de09d72c74c0
GeorgeKandelaki <kandelakig29@hotmail.com>
2025-05-07 23:00:03 +0400

    Boilerplate Code

--- server.js
+++ server.js
@@ -1,4 +1,14 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
+dotenv.config({ path: "./config.env" });
 const app = require("./app");
+
+const port = process.env.PORT || 3000;
+const connectionString = process.env.MONGODB_CONNECTION_STRING;
+
+mongoose.connect(connectionString).then(() => console.log("MongoDB Was Connected Successfully..."));
+
+const server = app.listen(port, () => {
+	console.log(`Server is Listening to requests on ${port}...`);
+});
